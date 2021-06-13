29b0529110795fd5ce31b585264f8ee166407d40
Soumyajit Mohapatra <hellosoumyajit@gmail.com>
2021-06-13 20:21:31 +0530

    logo changed

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import Footer from "./Footer"
 import Seo from "../components/Seo/seo"
 import GlobalStyles from "../styles/GlobalStyles"
 import TypographyStyles from "../styles/TypographyStyles"
-import Logo from "../images/Logo.png"
+import Logo from "../images/VerticalLogo.png"
 
 const Layout = ({ children }) => {
   return (
